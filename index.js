180ac03954f8569f2b49dcd9dd2a93eb144572a9
lucas <lucashtw@gmail.com>
2022-05-31 14:59:30 -0300

    instalando dependencias

---
+++ index.js
@@ -0,0 +1,16 @@
+/*const chalk = require('chalk');*/
+import chalk from 'chalk';
+
+
+console.log(chalk.blue('hello world!'));
+
+
+console.log('vamos começar!')
+
+const paragrafo = 'texto retornando por uma função';
+
+function texto(string){
+    return string
+}
+
+console.log(texto(paragrafo))
