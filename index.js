d2c0b45629bf891f45c33a395b5a7cae23970a78
lucas <lucashtw@gmail.com>
2022-07-29 14:27:39 -0300

    Finalizando tipos de tratamento de erro

--- index.js
+++ index.js
@@ -7,13 +7,28 @@ function trataErro(erro){
     throw new Error(chalk.red(erro.code,'não há arquivo no caminho'));
 }
 
-function pegaArquivo(caminhoDoArquivo){
+async function pegaArquivo(caminhoDoArquivo){
     const encoding = 'utf-8';
-    fs.promises
-    .readFile(caminhoDoArquivo, encoding)
-    .then((texto) => console.log(chalk.green(texto)))
-    .catch((erro) => trataErro(erro))
+    try {
+        const texto = await fs.promises.readFile(caminhoDoArquivo,encoding)
+        console.log(chalk.green(texto))
+        
+    } catch(erro){
+        trataErro(erro);
+    }
+
+
 }
+
+// function pegaArquivo(caminhoDoArquivo){
+//     const encoding = 'utf-8';
+//     fs.promises
+//     .readFile(caminhoDoArquivo, encoding)
+//     .then((texto) => console.log(chalk.green(texto)))
+//     .catch((erro) => trataErro(erro))
+// }
+
+//async await
 
 // function pegaArquivo(caminhoDoArquivo){
 //     const encoding = 'utf-8';
