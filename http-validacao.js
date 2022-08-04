1988d5475842f8d03f8031f3425c2d56f107eb78
lucas <lucashtw@gmail.com>
2022-08-04 09:54:33 -0300

    Montando o objeto link+status

--- http-validacao.js
+++ http-validacao.js
@@ -1,22 +1,39 @@
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args)); 
 
+function manejaErros(erro){
+    throw new Error(erro.message);
+}
+
 async function checaStatus(arrayURLs){
     //promises async await
-    const arrayStatus = await Promise.all(arrayURLs.map(async url =>{
-        const res = await fetch(url)
-        return res.status;
-    }))
-    return arrayStatus;
+    try{
+        const arrayStatus = await Promise
+            .all(arrayURLs
+                .map(async url =>{
+                    const res = await fetch(url)
+                    return res.status;
+        }))
+        return arrayStatus;
+    }catch(erro){
+        manejaErros(erro);
+    }
 }
 
 function geraArrayURLs(arrayLinks){
-    return arrayLinks.map(objetoLink => Object.values(objetoLink).join());
+    return arrayLinks
+        .map(objetoLink => Object
+            .values(objetoLink).join());
 }
 
 async function validaURLs(arrayLinks){
     const links =  geraArrayURLs(arrayLinks);
     const statusLinks = await checaStatus(links);
-    return statusLinks;
+    //spread operator
+    const resultados = arrayLinks.map((objeto,indice) => ({ 
+        ...objeto, 
+        status: statusLinks[indice] 
+    }))
+  return resultados;
 }
 
 module.exports = validaURLs;
