d4493fba8889483bc18d7be449555486fe83bd9c
lucas <lucashtw@gmail.com>
2022-08-08 13:14:09 -0300

    aplicando testes unitarios com jest

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,24 @@
 const pegaArquivo = require('../index');
 
-test('deve ser uma função', () => {
+const arrayResult = [
+  {
+    FileList: 'https://developer.mozilla.org/pt-BR/docs/Web/API/FileList'
+  }
+]
+
+describe('pegaArquivo::', () =>{
+  it('deve ser uma função',()=>{
     expect(typeof pegaArquivo).toBe('function');
-  });
+  })
+  it('deve retornar array com resultados', async () =>{
+    const resultado = await pegaArquivo('C:/Users/pc/Desktop/alura/NodeJS-criando-biblioteca/test/arquivos/texto1.md');
+    expect(resultado).toEqual(arrayResult);
+  })
+  it('deve retornar mensagem "não ha links"', async ()=>{
+    const resultado = await pegaArquivo('C:/Users/pc/Desktop/alura/NodeJS-criando-biblioteca/test/arquivos/texto1_semlinks.md');
+    expect(resultado).toBe('não há links');
+  })
+})
+
+
+
